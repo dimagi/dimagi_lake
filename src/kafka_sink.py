638887439512654452f9f8bdd396191a4f556675
Rohit Negi <rohit25.negi@gmail.com>
2021-02-24 07:01:58 +0530

    store forms by type

--- src/kafka_sink.py
+++ src/kafka_sink.py
@@ -1,16 +1,16 @@
 import json
-from src.pull_records import pull_cases_with_meta, pull_forms_with_meta, pull_ledgers_with_meta
+from src.pull_records import pull_cases_with_meta, pull_forms_with_meta
 from src.settings import (
     HQ_DATA_PATH,
     KAFKA_CASE_TOPIC,
     KAFKA_FORM_TOPIC,
     MAX_RECORDS_TO_PROCESS,
-    CHECKPOINT_BASE_DIR,
-    METASTORE_DB
+    CHECKPOINT_BASE_DIR
 )
 from corehq.apps.es import users
 from corehq.apps.locations.models import SQLLocation
-
+from corehq.util.json import CommCareJSONEncoder
+from collections import defaultdict
 
 class KafkaSink:
     spark_session = None
@@ -24,7 +24,6 @@ class KafkaSink:
         self.partition = partition
         self.bootstrap_server = bootstrap_server
         self.db_tables = [table.name for table in self.spark_session.catalog.listTables('default')]
-        self.table_name = self.topic.replace('-', '_')
         self.spark_session.sql("SET spark.databricks.delta.schema.autoMerge.enabled = true")
 
     def sink_kafka(self):
@@ -36,7 +35,8 @@ class KafkaSink:
                 return
             all_records = self.get_all_records(kafka_msgs_df.select('value').collect())
             self.merge_location_information(all_records)
-            self.bulk_merge(all_records)
+            records_by_type =  self.split_records_by_type(all_records)
+            self.bulk_merge(records_by_type)
             self.repartition_records()
 
         query = (kafka_messages.writeStream
@@ -62,58 +62,64 @@ class KafkaSink:
             return pull_forms_with_meta(record_metadata)
 
     def merge_location_information(self, records):
-        user_ids = [record['user_id'] for record in records]
+        user_ids = [record.get('user_id') or record.get('meta').get('userID') for record in records]
         user_with_loc = {user['_id']: user['location_id'] for user in (users.UserES()
                                                                         .user_ids(user_ids)
                                                                         .fields(['_id', 'location_id'])
                                                                         .run().hits)}
 
         for doc in records:
-            location_id = user_with_loc[doc['user_id']]
+            location_id = user_with_loc[doc.get('user_id') or doc.get('meta').get('userID')]
             ancestors = SQLLocation.by_location_id(location_id).get_ancestors(include_self=True)
-            location_info = dict()
             for loc in ancestors:
                 location_type = loc.location_type.name
-                location_info[f"{location_type}_id"] = loc.location_id
-                location_info[f"{location_type}_name"] = loc.name
+                doc[f"{location_type}_id"] = loc.location_id
+                doc[f"{location_type}_name"] = loc.name
 
-            doc['location_info'] = location_info
+    def split_records_by_type(self, all_records):
+        splitted_records = defaultdict(list)
+        for record in all_records:
+            splitted_records[record['type']].append(record)
+        return splitted_records
 
     def bulk_merge(self, all_records):
-        docs_df = (self.spark_session
-                   .read.json(self.spark_session
-                              .sparkContext.parallelize([json.dumps(doc).replace(": []",": [{}]") for doc in all_records])))
 
-        if self.topic in self.db_tables:
-            docs_df.createOrReplaceTempView(f"{self.table_name}_updates")
-            self.spark_session.sql(self.merge_query(existing_tablename=self.table_name,
-                                                    updates_tablename=f"{self.table_name}_updates"))
+        for records_type, records in all_records.items():
+            docs_df = (self.spark_session
+                       .read.json(self.spark_session
+                                  .sparkContext.parallelize([json.dumps(doc, cls=CommCareJSONEncoder).replace(": []", ": [{}]") for doc in records])))
 
-            # This commented Code is actually more straight forward way to merge data.
-            # But because the bug(fixed in https://github.com/apache/spark/pull/29667) it Throws error.
-            # This can be used when next version of spark is released with above fix.
-            #
-            # delta_table = DeltaTable.forPath(self.spark_session, records_table)
-            # delta_table.alias("existing_docs").merge(
-            #     docs_df.alias("incoming_docs"),
-            #     "existing_docs.type=incoming_docs.type and existing_docs._id = incoming_docs._id ") \
-            #     .whenMatchedUpdateAll() \
-            #     .whenNotMatchedInsertAll() \
-            #     .execute()
-        else:
-            print(f"New Table is being created with name {self.topic}")
-            docs_df.write.partitionBy('type', 'supervisor_id').saveAsTable(self.table_name,
-                                                                           format='delta',
-                                                                           mode='overwrite',
-                                                                           path=f"{HQ_DATA_PATH}/{self.topic}")
-            self.db_tables = [table.name for table in self.spark_session.catalog.listTables('default')]
+            table_name = f"{self.topic}_{records_type}".replace('-', '_')
+            if table_name in self.db_tables:
+                docs_df.createOrReplaceTempView(f"{table_name}_updates")
+                self.spark_session.sql(self.merge_query(existing_tablename=table_name,
+                                                        updates_tablename=f"{table_name}_updates"))
 
-        print(f"{len(docs)} docs were written to {self.topic}")
+                # This commented Code is actually more straight forward way to merge data.
+                # But because the bug(fixed in https://github.com/apache/spark/pull/29667) it Throws error.
+                # This can be used when next version of spark is released with above fix.
+                #
+                # delta_table = DeltaTable.forPath(self.spark_session, records_table)
+                # delta_table.alias("existing_docs").merge(
+                #     docs_df.alias("incoming_docs"),
+                #     "existing_docs.type=incoming_docs.type and existing_docs._id = incoming_docs._id ") \
+                #     .whenMatchedUpdateAll() \
+                #     .whenNotMatchedInsertAll() \
+                #     .execute()
+            else:
+                print(f"New Table is being created with name {self.topic}")
+                docs_df.write.partitionBy('type', 'supervisor_id').saveAsTable(table_name,
+                                                                               format='delta',
+                                                                               mode='append',
+                                                                               path=f"{HQ_DATA_PATH}/{self.topic}/{table_name}")
+                self.db_tables = [table.name for table in self.spark_session.catalog.listTables('default')]
+
+        print(f"{len(all_records)} docs were written to {self.topic}")
 
     def merge_query(self, existing_tablename, updates_tablename):
         return f"""
-        MERGE INTO "{existing_tablename}" existing_records 
-        USING "{updates_tablename}" updates 
+        MERGE INTO {existing_tablename} existing_records 
+        USING {updates_tablename} updates 
         ON existing_records.type = updates.type AND existing_records._id = updates._id 
         WHEN MATCHED THEN UPDATE SET * 
         WHEN NOT MATCHED THEN INSERT *
--- src/pull_records.py
+++ src/pull_records.py
@@ -17,14 +17,15 @@ def pull_forms_with_meta(forms_meta):
 
 def pull_cases_by_ids(case_ids):
     case_accessor = CaseAccessors(DATA_LAKE_DOMAIN)
-    return [case.to_json() for case in case_accessor.get_cases(case_ids)]
+    return [case.to_json() for case in case_accessor.get_cases(list(case_ids))]
 
 
 def pull_forms_by_ids(form_ids):
     form_accessor = FormAccessors(DATA_LAKE_DOMAIN)
-    forms_json = [form.to_json() for form in form_accessor.get_forms(form_ids)]
+    forms_json = [form.to_json() for form in form_accessor.get_forms(list(form_ids))]
 
     for form in forms_json:
         form['type'] = form['xmlns'].replace('http://openrosa.org/formdesigner/', '')
+        form['_id'] = form['meta']['instanceID']
     return forms_json
 
